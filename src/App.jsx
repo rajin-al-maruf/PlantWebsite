680120893e003056e57a3dec3023857739fe4145
Rajin Al Maruf <rajinalmarufananda@gmail.com>
2025-07-27 13:56:47 +0600

    asset and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div>
-      
+    <div className='px-10'>
+      <Navbar />
     </div>
   )
 }
 
 export default App
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../assets/BonomayaLogo.jpg'
+import { CgProfile } from 'react-icons/cg'
+import { CiHeart } from 'react-icons/ci'
+import { PiShoppingCartSimpleLight } from 'react-icons/pi'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='flex items-center gap-2'>
+        <img src={logo} className='w-14 rounded-full' alt="BonomayaLogo" />
+        {/* <h1 className='text-2xl font-bold'>BONOMAYA</h1> */}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Track Order</li>
+        <li>Contact Us</li>
+      </ul>
+
+      <div>
+        <CiHeart />
+        <PiShoppingCartSimpleLight/>
+        <CgProfile/>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
