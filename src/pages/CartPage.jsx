4312fa9173729e5215aa73b3ff3482d9f59a1ae6
Rajin Al-Maruf <rajinalmarufananda@gmail.com>
2025-09-21 14:55:01 +0600

    cartPage func

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,12 +5,24 @@ import useCartStore from "../store/cartStore";
 const CartPage = () => {
 
     const cart = useCartStore((state) => state.cart)
+    const removeFromCart = useCartStore((state) => state.removeFromCart)
+    const clearCart = useCartStore((state) => state.clearCart)
     const [quantity, setQuantity] = useState(1)
 
     const increase = () => setQuantity(quantity + 1)
     const decrease = () => {
         if (quantity > 1) setQuantity(quantity - 1)
     }
+
+    let subtotal = 0;
+    const calSubtotal = () => {
+        for (let index = 0; index < cart.length; index++) {
+               subtotal = subtotal+cart[index].price
+        }
+    }
+    calSubtotal();
+    const shipping = 100;
+    let total = subtotal+shipping;
     
   return (
     <div className='max-w-7xl mx-auto mt-36 px-4 md:px-6 lg:px-8 xl:px-0'>
@@ -19,16 +31,19 @@ const CartPage = () => {
         <div className='mt-6 grid grid-cols-1 lg:grid-cols-3'>
             <div className='col-span-2 md:px-10'>
                 <h2 className='p-4 border-b border-neutral-300 text-lg font-medium'>Cart Items</h2>
-                {cart.map((item) => {
+                {cart.map((item, index) => {
                     return(
-                        <div className='flex gap-4 p-4 border-t border-neutral-300'>
-                            <img 
-                                src={item.imgurl} 
-                                alt={item.name}
-                                className='w-20 sm:w-28 md:w-32 bg-neutral-100' />
+                        <div key={index} className='flex gap-4 p-4 border-t border-neutral-300'>
+                            <Link to={`/product/${item.id}`}>
+                                <img 
+                                    src={item.imgurl} 
+                                    alt={item.name}
+                                    className='w-20 sm:w-28 md:w-32 bg-neutral-100' 
+                                />
+                            </Link>
                             <div className="w-full flex justify-between">
                                 <div>
-                                    <p className='font-medium'>{item.name}</p>
+                                    <Link to={`/product/${item.id}`}><p className='font-medium'>{item.name}</p></Link>
                                     <p className='text-xs'>{item.availability}</p>
                                     <p className='text-xs mt-2'>Care Level: {item.carelevel}</p>
                                     <div className="flex items-center gap-2 sm:gap-4 mt-2">
@@ -50,13 +65,25 @@ const CartPage = () => {
                                 </div>
                                 <div className="flex flex-col justify-between items-end">
                                     <p className="text-sm sm:text-base font-medium">Tk {item.price}</p>
-                                    <p className="text-xs underline cursor-pointer">remove</p>
+                                    <button 
+                                        onClick={() => removeFromCart(item.id)}
+                                        className="text-xs underline cursor-pointer"
+                                    >
+                                        remove
+                                    </button>
                                 </div>
                             </div>
 
                         </div>
                     )
                 })}
+                {cart.lenght>0 &&
+                    <button 
+                        onClick={() => clearCart()}
+                        className="text-xs underline cursor-pointer">
+                        Clear Cart
+                    </button>
+                }
             </div>
 
             <div className="md:px-10 lg:border-l border-l-neutral-300">
@@ -64,24 +91,24 @@ const CartPage = () => {
                 <div className='p-4 border-b border-neutral-300'>
                     <div className="flex justify-between text-sm">
                         <p>Number of Items</p>
-                        <p>2</p>
+                        <p>{cart.length}</p>
                     </div>
                     <div className="flex justify-between text-sm pt-2">
                         <p>Subtotal</p>
-                        <p>Tk 750</p>
+                        <p>Tk {subtotal}</p>
                     </div>
                     <div className="flex justify-between text-sm pt-2">
                         <p>Shipping</p>
-                        <p>Tk 100</p>
+                        <p>Tk {shipping}</p>
                     </div>
-                    <div className="flex justify-between text-sm pt-2">
+                    {/* <div className="flex justify-between text-sm pt-2">
                         <p>VAT</p>
                         <p>Tk 33.84</p>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex justify-between text-sm font-medium p-4 border-b border-neutral-300">
                     <p>Total</p>
-                    <p>Tk 883.84</p>
+                    <p>Tk {total}</p>
                 </div>
                 <Link to="/checkout"> 
                     <button className='w-full p-2 text-white text-sm bg-black hover:bg-brand-primary duration-200 mt-4 rounded-md cursor-pointer'>
--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -7,9 +7,11 @@ const useCartStore = create((set) => ({
             if (state.cart.find((item) => item.id === product.id)) return state;
             return { cart: [...state.cart, { ...product, quantity: 1 }] };
         }),
-    removeFromCart: (id) => 
-        set((state) => {state.cart.filter((item) => item.id !== id)}),
+    removeFromCart: (id) =>
+        set((state) => ({
+            cart: state.cart.filter((item) => item.id !== id)
+        })),
     clearCart: () => set({ cart: [] }),
-}))
+}));
 
 export default useCartStore
